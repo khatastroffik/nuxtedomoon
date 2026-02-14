79cbe88236a22f184fb4518a28df311d8edeb13f
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2026-02-14 13:09:04 +0100

    chore: add @antfu/eslint-config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  formatters: true,
+  vue: true,
+  type: "app",
+  typescript: true,
+  stylistic: {
+    indent: 2,
+    quotes: "double",
+    semi: true,
+  },
+  rules: {
+    "ts/no-redeclare": "off",
+    "vue/max-attributes-per-line": ["error", { singleline: { max: 2 }, multiline: { max: 1 } }],
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "unicorn/filename-case": ["error", { case: "kebabCase", ignore: ["README.md"] }],
+    // "perfectionist/sort-imports": ["error", { tsconfigRootDir: "." }],
+  },
+  ignores: ["CHANGELOG.md", ".pnpm-store/**", "**/migrations/*"],
+});
