15c0b97a508a8094d416f3f031ca47bbf517d3b4
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2026-02-16 12:16:22 +0100

    chore(#3): replace @nuxtjs/tailwindcss module with standard tailwindcss package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   typescript: { typeCheck: false }, // https://github.com/fi3ework/vite-plugin-checker/issues/557
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
-  tailwindcss: {
-    cssPath: ["~/assets/style/main.css", { injectPosition: "first" }],
-    config: {},
-    editorSupport: true,
-    viewer: true,
-    exposeConfig: true,
-  },
+  modules: ["@nuxt/content"],
+  // tailwindcss: {
+  //   cssPath: ["~/assets/style/main.css", { injectPosition: "first" }],
+  //   config: {},
+  //   editorSupport: true,
+  //   viewer: true,
+  //   exposeConfig: true,
+  // },
   css: ["~/assets/style/main.css"],
   content: {
     watch: {
@@ -26,5 +28,9 @@ export default defineNuxtConfig({
         },
       },
     },
+  },
+  vite: {
+    // @ts-expect-error plugin's interface is faulty (see https://github.com/tailwindlabs/tailwindcss/issues/18802)
+    plugins: [tailwindcss()],
   },
 });
