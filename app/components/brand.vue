8eac64d1991efd31e7a87568a0ab06923817ad08
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2026-02-18 22:24:28 +0100

    chore(#3): disable lint rule " vue/singleline-html-element-content-newline"

--- app/components/brand.vue
+++ app/components/brand.vue
@@ -4,13 +4,8 @@
 
 <template>
   <NuxtLink to="/" class="flex font-sans text-2xl font-semibold text-base-content drop-shadow-sm hover:drop-shadow-neutral-content/60">
-    <div class="rotate-y-180 skew-y-20">
-      K
-    </div><div class="font-normal opacity-50 hover:opacity-80">
-      HATASTROFFI
-    </div><div class="skew-y-20">
-      K
-    </div>
-    <!-- <div class="rotate-y-180">K</div><div class="opacity-60 hover:opacity-80 text-xl self-center-safe">HATA<span class="blink_me">STR</span>OFFI</div><div>K</div>     -->
+    <div class="rotate-y-180 skew-y-20">K</div>
+    <div class="font-normal opacity-50 hover:opacity-80">HATASTROFFI</div>
+    <div class="skew-y-20">K</div>
   </NuxtLink>
 </template>
