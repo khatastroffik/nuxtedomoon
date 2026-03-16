cf96d96adcf615320cc808d8c42d1bef67c63ee5
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2026-03-16 02:05:35 +0100

    feat(#17): add "stats-card" and "divider" component for landing page

---
+++ app/components/stats-card.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="flex w-xs max-w-xs flex-col flex-wrap content-center items-center px-4 py-2 sm:items-end">
+    <div class="inline-flex items-center justify-center gap-1 xl:justify-end">
+      <h2 class="text-3xl font-semibold text-secondary sm:text-4xl md:text-6xl xl:text-8xl">
+        <slot name="value" />
+      </h2>
+      <h2 class="text-3xl font-extrabold text-accent sm:text-4xl md:text-7xl xl:text-6xl">+</h2>
+    </div>
+    <h4 class="text-center text-sm text-ellipsis sm:text-lg xl:text-end xl:text-2xl">
+      <slot name="description" />
+    </h4>
+  </div>
+</template>
