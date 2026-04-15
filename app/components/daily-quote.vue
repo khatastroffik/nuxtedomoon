682b384df1e90178a517d2d3da4fda16b386ff63
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2026-04-15 10:25:14 +0200

    fix(#36): update the quote client side

--- app/components/daily-quote.vue
+++ app/components/daily-quote.vue
@@ -1,18 +1,30 @@
 <script lang="ts" setup>
-const { data } = await useAsyncData(`quotes`, () => {
-  return queryCollection<"quotes">("quotes").first();
+const IsNotDigit = /\D/g;
+const IsLineBreak = /(\r\n|\r|\n)/g;
+const quoteOfTheDay = ref<{ quote: string; author: string; cite?: string }>({ quote: "To attain knowledge, add things everyday.\nTo obtain wisdom, remove things everyday.", author: "Lao Tzu", cite: undefined });
+
+const { data: quotes } = await useAsyncData(`quotes`, () => {
+  return queryCollection<"quotes">("quotes").first().then(item => item?.quotes);
 });
-const today = new Date().toISOString().slice(0, 10).replace(/\D/g, "");
-const quotes = data.value?.quotes ?? [];
-const quoteIndex = Number.parseInt(today) % quotes.length;
-const quote = quotes.at(quoteIndex);
+
+function formatQuoteSource(q: typeof quoteOfTheDay.value) {
+  return `&#x203A; ${q.author}${q.cite ? ` (${q.cite})` : ""} &#x2039;`;
+}
+
+onMounted(() => {
+  const quoteIndex = Number.parseInt(new Date().toISOString().slice(0, 10).replace(IsNotDigit, "")) % (quotes.value?.length || 1);
+  const q = quotes.value?.at(quoteIndex);
+  if (q) {
+    quoteOfTheDay.value = { quote: q.quote.replace(IsLineBreak, "<br>"), author: q.author, cite: q.cite };
+  }
+});
 </script>
 
 <template>
-  <div class="my-4 max-w-5xl px-8 pt-2 text-base-content/50">
+  <div class="my-4 max-w-5xl px-8 pt-2 text-base-content/50" aria-label="quote of the day">
     <blockquote class="mb-2">
-      <p class="mb-0" v-html="quote?.quote.replace(/(\r\n|\r|\n)/g, '<br>')" />
+      <p class="mb-0" aria-label="quote" v-html="quoteOfTheDay.quote" />
     </blockquote>
-    <p class="mt-0" v-html="`&#x203A; ${quote?.author} &#x2039;`" />
+    <p class="mt-0" aria-label="author" v-html="formatQuoteSource(quoteOfTheDay)" />
   </div>
 </template>
